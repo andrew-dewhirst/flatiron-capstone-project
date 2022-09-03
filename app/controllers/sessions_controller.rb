d9adb0bddc20ca3575d82ab41659673a758ddaca
Andrew Dewhirst <andrew.dewhirst8@gmail.com>
2022-09-03 16:08:26 -0400

    Added destroy method to Sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,4 +9,14 @@ class SessionsController < ApplicationController
       render json: { errors: ["Invalid username or password"] }, status: :unauthorized
     end
   end
+
+  def destroy
+    user = User.find_by(id: session[:user_id])
+    if user
+      session.delete :user_id
+      head :no_content
+    else
+      render json: { errors: ["Not logged in"] }, status: :unauthorized
+    end
+  end
 end
