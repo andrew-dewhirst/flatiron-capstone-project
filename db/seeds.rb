d2cda208b090a281018a4d62b4e8707f7aef0b66
Andrew Dewhirst <andrew.dewhirst8@gmail.com>
2022-09-22 18:48:50 -0400

    Added Active Storage

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+andrew = User.create(
+  first_name: "Andrew",
+  last_name: "Dewhirst",
+  email: "andrew@example.com",
+  birthday: "07/01/2000",
+  username: "adewhirst",
+  password: "password",
+  password_confirmation: "password"
+)
+
+andrew.avatar.attach(
+  io: File.open('./public/avatars/kid.png'),
+  filename: 'kid.png',
+  content_type: 'application/png'
+)
