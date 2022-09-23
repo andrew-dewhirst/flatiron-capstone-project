f809eb4b53a6dfcc448dc54efe7acfd67ae3da12
Andrew Dewhirst <andrew.dewhirst8@gmail.com>
2022-09-23 15:54:32 -0400

    Outlined rough framework for App component

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,8 +3,11 @@ class UsersController < ApplicationController
   rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
 
   def index
-    users = User.all
-    render json: users
+    # users = User.all
+    users = User.first
+    avatar = rails_blob_path(users.avatar)
+    # render json: users
+    render json: { users: users, avatar: avatar }
   end
   
   def create
@@ -15,8 +18,9 @@ class UsersController < ApplicationController
 
   def show
     user = User.find_by(id: session[:user_id])
+    avatar = rails_blob_path(user.avatar)
     if user
-      render json: user, status: :created
+      render json: { user: user, avatar: avatar }, status: :created
     else
       render json: { error: "Invalid username or password" }, status: :unauthorized
     end
