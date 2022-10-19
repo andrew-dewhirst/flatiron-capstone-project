3aef9a68d5299dfd794672d6d365f08b9b997314
Andrew Dewhirst <andrew.dewhirst8@gmail.com>
2022-10-19 13:07:51 -0400

    Built generic framework using my existing code

--- client/src/App.js
+++ client/src/App.js
@@ -1,95 +1,85 @@
 import React, { useEffect, useState } from "react";
 import { Route, Switch } from "react-router-dom";
-// import NavBar from "./NavBar";
-// import Login from "./Login";
-// import Home from "./Home";
-// import RenovationList from "./RenovationList";
-// import NewRenovation from "./NewRenovation";
-// import MyRenovation from "./MyRenovation";
+import NavBar from "./NavBar";
+import Login from "./Login";
+import Home from "./Home";
+import RenovationList from "./RenovationList";
+import NewRenovation from "./NewRenovation";
+import MyRenovation from "./MyRenovation";
 
 function App() {
-  const [user, setUser] = useState({});
-  // const [renovations, setRenovations] = useState([]);
-  // const [errors, setErrors] = useState([]);
-
-  // useEffect(() => {
-  //   fetch("/me").then((r) => {
-  //     if (r.ok) {
-  //       r.json().then((user) => setUser(user));
-  //     }
-  //   });
-  // }, []);
+  const [user, setUser] = useState(null);
+  const [renovations, setRenovations] = useState([]);
+  const [errors, setErrors] = useState([]);
 
   useEffect(() => {
-    fetch("/cards")
-      .then((r) => r.json())
-      .then((user) => setUser(user));
+    fetch("/me").then((r) => {
+      if (r.ok) {
+        r.json().then((user) => setUser(user));
+      }
+    });
   }, []);
 
-  console.log(user)
+  useEffect(() => {
+    fetch("/renovations")
+      .then((r) => r.json())
+      .then((renovation) => setRenovations(renovation));
+  }, []);
 
 
+  function handleLogoutClick() {
+    fetch("/logout", { method: "DELETE" }).then((r) => {
+      if (r.ok) {
+        setUser(null);
+      }
+    });
+  }
 
-  // function handleLogoutClick() {
-  //   fetch("/logout", { method: "DELETE" }).then((r) => {
-  //     if (r.ok) {
-  //       setUser(null);
-  //     }
-  //   });
-  // }
+  function handleNewRenovation(newRenovation) {
+    setRenovations([...renovations, newRenovation])
+  };
 
-  // function handleNewRenovation(newRenovation) {
-  //   setRenovations([...renovations, newRenovation])
-  // };
+  function handleRenovationDelete(deletedRenovation) {
+    const updatedRenovations = renovations.filter((renovation) => renovation.id !== deletedRenovation)
+    setRenovations(updatedRenovations);
+  }
 
-  // function handleRenovationDelete(deletedRenovation) {
-  //   const updatedRenovations = renovations.filter((renovation) => renovation.id !== deletedRenovation)
-  //   setRenovations(updatedRenovations);
-  // }
+  function handleUpdateRenovation(updatedRenovation) {
+    const updatedRenovationArray = renovations.map((renovation) => {
+      return renovation.id === updatedRenovation.id ? updatedRenovation : renovation;
+    });
+    setRenovations(updatedRenovationArray);
+  }
 
-  // function handleUpdateRenovation(updatedRenovation) {
-  //   const updatedRenovationArray = renovations.map((renovation) => {
-  //     return renovation.id === updatedRenovation.id ? updatedRenovation : renovation;
-  //   });
-  //   setRenovations(updatedRenovationArray);
-  // }
-
-  // if (!user) return (
-  //   <div>
-  //     <h3>{errors}</h3>
-  //     <Login onLogin={setUser} setErrors={setErrors} />;
-  //   </div>
-  // )
-
-  // return (
-  //   <div>
-  //     <NavBar user={user} setUser={setUser} />
-  //     <button type="button" onClick={handleLogoutClick}>
-  //         Logout
-  //    </button>
-  //     <Switch>
-  //     <Route exact path="/">
-  //         <Home user={user}/>
-  //       </Route>
-  //       <Route exact path="/renovations">
-  //         <RenovationList renovations={renovations} user={user} handleUpdateRenovation={handleUpdateRenovation} />
-  //       </Route>
-  //       <Route exact path="/new_renovation">
-  //         <NewRenovation user={user} renovations={renovations} handleNewRenovation={handleNewRenovation} />
-  //       </Route>
-  //       <Route exact path="/my_renovations">
-  //         <MyRenovation user={user} renovations={renovations} handleRenovationDelete={handleRenovationDelete} handleUpdateRenovation={handleUpdateRenovation} />
-  //       </Route>
-  //     </Switch>
-  //   </div>
-  // );
+  if (!user) return (
+    <div>
+      <h3>{errors}</h3>
+      <Login onLogin={setUser} setErrors={setErrors} />;
+    </div>
+  )
 
   return (
     <div>
-      Testing
-      {/* <img src={`http://localhost:3000${user.avatar}`} /> */}
+      <NavBar user={user} setUser={setUser} />
+      <button type="button" onClick={handleLogoutClick}>
+          Logout
+     </button>
+      <Switch>
+      <Route exact path="/">
+          <Home user={user}/>
+        </Route>
+        <Route exact path="/renovations">
+          <RenovationList renovations={renovations} user={user} handleUpdateRenovation={handleUpdateRenovation} />
+        </Route>
+        <Route exact path="/new_renovation">
+          <NewRenovation user={user} renovations={renovations} handleNewRenovation={handleNewRenovation} />
+        </Route>
+        <Route exact path="/my_renovations">
+          <MyRenovation user={user} renovations={renovations} handleRenovationDelete={handleRenovationDelete} handleUpdateRenovation={handleUpdateRenovation} />
+        </Route>
+      </Switch>
     </div>
-  )
+  );
 }
 
 export default App;
--- client/src/NavBar.js
+++ client/src/NavBar.js
@@ -5,7 +5,7 @@ function NavBar() {
 
   const navStyle = {
     display: "inline-block",
-    width: "25%",
+    width: "15%",
     padding: "12px",
     margin: "0 4% 6px",
     background: "black",
@@ -22,47 +22,26 @@ function NavBar() {
       >
         Home
       </NavLink>
-      <NavLink to ="/cards/pokemon"
+      <NavLink to ="/renovations"
       exact
       style={navStyle}
       activeStyle={{background: "#696969"}}
       >
-        Pokemon
+        View All Renovations
       </NavLink>
-      <NavLink to ="/cards/mlb"
+      <NavLink to ="/my_renovations"
       exact
       style={navStyle}
       activeStyle={{background: "#696969"}}
       >
-        MLB
+        View My Renovations
       </NavLink>
-      <NavLink to ="/cards/nba"
+      <NavLink to ="/new_renovation"
       exact
       style={navStyle}
       activeStyle={{background: "#696969"}}
       >
-        NBA
-      </NavLink>
-      <NavLink to ="/cards/nhl"
-      exact
-      style={navStyle}
-      activeStyle={{background: "#696969"}}
-      >
-        NHL
-      </NavLink>
-      <NavLink to ="/cards/nfl"
-      exact
-      style={navStyle}
-      activeStyle={{background: "#696969"}}
-      >
-        NFL
-      </NavLink>
-      <NavLink to ="/myaccount"
-      exact
-      style={navStyle}
-      activeStyle={{background: "#696969"}}
-      >
-        My Account
+        Create New Renovation
       </NavLink>
     </div>
   )
