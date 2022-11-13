6e65abfb745ad8c4047d5d8281599b09f9496199
Andrew Dewhirst <andrew.dewhirst8@gmail.com>
2022-11-13 11:09:52 -0500

    Added Shopping Cart icon to page

--- client/src/Account.js
+++ client/src/Account.js
@@ -1,11 +1,18 @@
 import React, { useEffect, useState } from "react";
 import Login from "./Login";
+import ButtonGroup from "@material-ui/core/ButtonGroup";
+import Badge from "@material-ui/core/Badge";
+import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
+import Button from "@material-ui/core/Button";
+import AddIcon from "@material-ui/icons/Add";
+import RemoveIcon from "@material-ui/icons/Remove";
 
 function Account({ user, avatar }) {
 
   const [errors, setErrors] = useState([]);
   console.log(user.user)
   console.log(user)
+  const [itemCount, setItemCount] = React.useState(1);
 
   // if (!user) return (
   //   <div>
@@ -14,10 +21,35 @@ function Account({ user, avatar }) {
   //   </div>
   // )
   return (
-    <div>
-      Welcome Home {user?.user?.first_name} {user?.user?.last_name}!
+    <div style={{ display: "block", padding: 30 }}>
+      <h4>How to create ShoppingCart Button in ReactJS?</h4>
+      <div>
+        <Badge color="secondary" badgeContent={itemCount}>
+          <ShoppingCartIcon />{" "}
+        </Badge>
+        <ButtonGroup>
+          <Button
+            onClick={() => {
+              setItemCount(Math.max(itemCount - 1, 0));
+            }}
+          >
+            {" "}
+            <RemoveIcon fontSize="small" />
+          </Button>
+          <Button
+            onClick={() => {
+              setItemCount(itemCount + 1);
+            }}
+          >
+            {" "}
+            <AddIcon fontSize="small" />
+          </Button>
+        </ButtonGroup>
+      </div>
     </div>
   );
-};
+}
 
 export default Account
+
+// Welcome Home {user?.user?.first_name} {user?.user?.last_name}!
